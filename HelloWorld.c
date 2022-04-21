ca095d22dbce09651ff0fccb1c2273c3a1d6fdca
SunYoung-P <p2sy45@gmail.com>
2022-04-21 21:45:04 +0900

    start
    vscode
    C compile, execute, debug 환경 구축

---
+++ HelloWorld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int i = 0;
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("i value : %d \n", i);
+    }
+    printf("Hello World !");
+    return 0;
+}
