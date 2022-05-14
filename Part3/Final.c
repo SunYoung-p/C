ef5c1240c610ece619e43f908d68cdb220bd6082
SunYoung-P <p2sy45@gmail.com>
2022-05-15 01:17:19 +0900

    solve part3 final problem-no3~no5

--- Part3/Final.c
+++ Part3/Final.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void No1_PrintArr(int, int[][4]);
 void No1();
 void No2();
+void No3();
+void No4();
+void No5();
 
 int main()
 {
@@ -96,4 +101,59 @@ void No2()
         }
         puts("");
     }
+}
+
+void No3()
+{
+    for (int i = 0; i < 5; i++)
+        printf("%d번째 난수 : %d (0이상 99이하) \n", i + 1, rand() % 100);
+}
+
+void No4()
+{
+    srand((int)time(NULL)); // 시드 입력
+
+    printf("주사위 1의 결과 : %d \n", rand() % 6 + 1);
+    printf("주사위 2의 결과 : %d \n", rand() % 6 + 1);
+}
+
+void No5()
+{
+    int n = 0, m = 0, o = 0, p = 0, re = 0;
+
+    srand((int)time(NULL));
+
+    do
+    {
+        printf("바위는 1, 가위는 2, 보는 3: ");
+        scanf("%d", &o);
+        p = rand() % 3 + 1;
+
+        printf("당신은 %s 선택, 컴퓨터는 %s 선택, ",
+               o == 1 ? "바위" : o == 2 ? "가위"
+                             : o == 3   ? "보"
+                                        : "잘못 입력했습니다",
+               p == 1 ? "바위" : p == 2 ? "가위"
+                             : p == 3   ? "보"
+                                        : "잘못된 값");
+
+        if (o == p)
+        {
+            printf("비겼습니다! \n");
+            m++;
+        }
+        else if ((o == 1 && p == 3) || (o == 2 && p == 1) || (o == 3 && p == 2))
+        {
+            printf("당신이 졌습니다! \n");
+            re++;
+        }
+        else
+        {
+            printf("당신이 이겼습니다! \n");
+            n++;
+        }
+
+    } while (re == 0);
+
+    printf("\n게임의 결과 : %d승, %d무 \n", n, m);
 }
